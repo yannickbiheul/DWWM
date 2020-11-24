a0a4251fa4505b8ba25b07e7ff5f594ed8a49ff8
Yannick Biheul <yannickbiheul@outlook.fr>
2020-11-24 07:38:22 +0100

    Init fichiers horloge

---
+++ PROJET Horloge/js/horloge.js
@@ -0,0 +1,14 @@
+// var compteur_minutes=0;
+	// var compteur_heures=0;
+
+	// function tourne()
+	// 	{
+// 1) utiliser la fonction setTimeout( uneFonction(){} , 1000 ) => execute la fonction dans une seconde
+// 2) modulo sur un entier   =>   1 % 40 = 1  , 2 % 12 = 2 ...etc
+// 3) modifier la fonction pour l'effectuer avec setInterval() 
+//	https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_clearinterval
+
+
+// 		if (indic) window.setTimeout("tourne()", "1000");
+// .....
+// 		}
