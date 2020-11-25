a8729dd58d6a99f056e137f7788be5b371edcae1
Yannick Biheul <yannickbiheul@outlook.fr>
2020-11-25 14:51:34 +0100

    essai toggle en jquery sur menuBurger

--- MEDIA QUERIES/js/burger.js
+++ MEDIA QUERIES/js/burger.js
@@ -1,30 +1,4 @@
-
-
 $(".boutonBurger").click(function() {
-
-    if ($(".boutonBurger").css("display") == "flex") {
-
-        if ($("nav").css("display") == "none") {
-
-            $("nav").css({
-                display: "flex"
-            })
-
-        } else {
-
-            if ($(".boutonBurger").css("display") == "flex") {
-                $("nav").css({
-                    display: "none"
-                })
-            } else {
-                $("nav").css("display") == "flex";
-            }
-            
-        }
-
-    } else {
-        $("nav").css("dsiplay") == "flex";
-    }
-    
+    $("nav").toggle(".nav2");
 })
 
