ab5041ebbd49fa05727f105343eb81156fbff512
Yannick Biheul <yannickbiheul@outlook.fr>
2020-11-26 08:46:39 +0100

    1er essai compétences json

--- PROJET 1 - CV/js/cvGrid.js
+++ PROJET 1 - CV/js/cvGrid.js
@@ -12,3 +12,52 @@ for (let i = 0; i < elts.length; i++) {
 function toggleMenu() {
     menuBurger.classList.toggle("menuBurgerOpen");
 }
+
+/* Compétences en json */
+
+let langages = {
+    nom: "Langages",
+    l1: "HTML",
+    l2: "CSS",
+    l3: "JavaScript",
+    l4: "Json"
+};
+
+let frameworks = {
+    nom: "Frameworks",
+    f1: "Bootstarp",
+    f2: "jQuery"
+};
+
+let outils = {
+    nom: "Outils",
+    o1: "Visual Studio Code",
+    o2: "Git / GitHub",
+    o3: "Filezilla"
+};
+
+let tabComp = [langages, frameworks, outils];
+console.log(tabComp);
+let clefs = Object.keys(tabComp);
+let valeurs = Object.values(tabComp);
+
+
+let containerLangages = document.querySelectorAll(".langages");
+
+
+let tab = JSON.stringify(tabComp);
+
+
+let tabListe = [];
+
+console.log(valeurs[0].length);
+
+for (let i = 0; i < containerLangages.length; i++) {
+    $(containerLangages[i]).append(`<h3>${tabComp[i].nom}</h3>`);
+    $(containerLangages[i]).append(`<ul class="liste"></<ul>`);
+    console.log(tabComp[i].length);
+    for (let j = 0; j < tabComp[j].length; j++) {
+        console.log("Coucou !");
+    }
+}
+
