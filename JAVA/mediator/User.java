1683cddb7ba8f4ad9b9d735e858a408b5225c282
29010-33-01 <29010-33-01@exchange.ad.afpanet>
2020-12-07 12:41:09 +0100

    Ajout de mediator dans JAVA

---
+++ JAVA/mediator/User.java
@@ -0,0 +1,25 @@
+package mediator;
+
+public class User {
+
+    // DATAS
+    private String name;
+
+    // METHODES
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void sendMessage(String message) {
+        ChatRoom.showMessage(this, message);
+    }
+    
+}
