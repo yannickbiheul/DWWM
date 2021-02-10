e05ff115b862c005d319a0d177b461db3a4097b7
Yannick Biheul <yannickbiheul@outlook.fr>
2021-02-10 16:28:35 +0100

    fin 1er_script

--- VUE.JS/1er_script/js/main.js
+++ VUE.JS/1er_script/js/main.js
@@ -1,7 +1,56 @@
+                                        /* ----- COMPOSANT TODO-ITEM ----- */
+
 Vue.component('todo-item', {
     props: ['todo'],
     template: '<li>{{todo.text}}</li>'
 })
+
+                                        /* ----- COMPOSANT MYCOMPONENT ----- */
+
+Vue.component('mycomponent', {
+    data: function() {
+        return {
+            title: "Mon Composant !"
+        }
+    },
+    template: '<h1>{{title}}</h1>'
+})
+
+                                        /* ----- COMPOSANT ESSAIPROP ----- */
+
+Vue.component('essaiprop', {
+    props: ['title', 'mytext'],
+    template: '<h3 v-on:click="clickTest">{{title}}</h3>',
+    methods: {
+        clickTest: function() {
+            console.log(this.myText);
+        }
+    }
+})
+
+                                        /* ----- COMPOSANT NEWCOMPONENT ----- */
+
+Vue.component('newcomponent', {
+    props: ['montitre', 'mytext'],
+    template: '<h3 v-on:click="clickTest">{{montitre}}</h3>',
+    methods: {
+        clickTest: function () {
+            console.log(this.mytext);
+        }
+    }
+})
+
+Vue.component('uneboucle', {
+    props: ['montitre', 'mytext'],
+    template: '<h3 v-on:click="clickTest">{{montitre}}</h3>',
+    methods: {
+        clickTest: function () {
+            console.log(this.mytext);
+        }
+    }
+})
+
+                                                            /* ----- SECTION 1 ----- */
 
 new Vue({
     el: "#app",
@@ -11,29 +60,15 @@ new Vue({
         seen: false,
         strongTag: "<strong>IMPORTANT</strong>",
         date: "Nous sommes le " + new Date().toLocaleString(),
-        compteur: 0,
-        tagName: "rien"
-    },
-    computed: {
-        wordComputed: function() {
-            return this.compteur < 0 ? "Négatif" : "Positif"
-        }
     },
     methods: {
         maFonction: function() {
             this.titre = this.titre + " ça va ?"
-        },
-        increment: function() {
-            this.compteur++
-        },
-        decrement: function() {
-            this.compteur--;
-        },
-        hover: function(event) {
-            this.tagName = event.target.tagName
         }
     }
 })
+
+                                                            /* ----- SECTION 2 ----- */
 
 new Vue({
     el: "#app2",
@@ -52,6 +87,8 @@ new Vue({
     }
 })
 
+                                                            /* ----- SECTION 3 ----- */
+
 new Vue({
     el: "#app3",
     data: {
@@ -67,5 +104,46 @@ new Vue({
             { id: 2, text: "Fromage" },
             { id: 3, text: "Bières" }
         ]
+    }
+})
+
+                                                            /* ----- SECTION 4 ----- */
+
+new Vue({
+    el: "#app4",
+    data: {
+        compteur: 0,
+        tagName: "rien"
+    },
+    computed: {
+        wordComputed: function () {
+            return this.compteur < 0 ? "Négatif" : "Positif"
+        }
+    },
+    methods: {
+        increment: function () {
+            this.compteur++
+        },
+        decrement: function () {
+            this.compteur--;
+        },
+        hover: function (event) {
+            this.tagName = event.target.tagName
+        }
+    }
+})
+
+                                                            /* ----- SECTION 5 ----- */
+
+new Vue({
+    el: "#app5",
+    data: {
+        titleData: "Hello from data",
+        titresData: ['Texte 1', 'Texte 2', 'Texte 3', 'Texte 4', 'Texte 5', 'Texte 6']
+    },
+    methods: {
+        clickTest: function() {
+            this.titleData = "eh ben je change !"
+        }
     }
 })
